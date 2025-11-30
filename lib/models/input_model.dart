88fa58070e1e68bd3d055b9e9a65ef222638a55a
Allan Monteiro <allanbrados@gmail.com>
2025-11-30 00:56:53 -0300

    fix preset

--- lib/models/input_model.dart
+++ lib/models/input_model.dart
@@ -10,7 +10,7 @@ class InputModel extends ChangeNotifier {
     Matrix4(
       0, 1, 0, 0,
       1, 0, 0, 0,
-      0, 1, 0, 0,
+      0, 0, 1, 0,
       0, 0, 0, 1),
     Matrix4(
       0, 0, 1, 0,
